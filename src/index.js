2a396c91d99a62f96f148cbf0a53b8bd16a11efc
Vlad Prodan <vladprodan6@gmail.com>
2022-01-24 15:24:39 +0200

    Preparation

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './logo.svg';
+import printMe from './print.js';
 
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack', 'sykaaa'], ' ');
-    element.classList.add('hello');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    const myIcon = new Image();
-    myIcon.src = Icon;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-    element.appendChild(myIcon);
+    element.appendChild(btn);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-        ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
     },
 };
