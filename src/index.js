8e3ac0fb812b7d9db24bbeff65bd561c7678bcab
Vlad Prodan <vladprodan6@gmail.com>
2022-01-24 15:15:43 +0200

    Loading Images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './logo.svg';
 
 
 function component() {
@@ -8,6 +9,11 @@ function component() {
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack', 'sykaaa'], ' ');
     element.classList.add('hello');
+
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
 }
