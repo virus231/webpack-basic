cb79c1505a245ee14206a6559c6d36f837f1e8cc
Vlad Prodan <vladprodan6@gmail.com>
2022-01-24 15:31:44 +0200

    Setting up HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
         index: './src/index.js',
         print: './src/print.js',
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'main.html',
+            template: 'src/index.html',
+        })
+    ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
