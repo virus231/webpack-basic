e18e6daf2b6ab7a97aa91083c5dc15811c1fc41a
Vlad Prodan <vladprodan6@gmail.com>
2022-01-26 16:03:21 +0200

    Prevent Duplication

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,15 @@ module.exports = {
     mode: 'development',
     mode: 'development',
     entry: {
-        index: './src/index.js',
-        another: './src/another-module.js',
+        index: {
+            import: './src/index.js',
+            dependOn: 'shared',
+        },
+        another: {
+            import: './src/another-module.js',
+            dependOn: 'shared',
+        },
+        shared: 'lodash',
     },
     devtool: 'inline-source-map',
     devServer: {
@@ -22,5 +29,8 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
     },
 };
